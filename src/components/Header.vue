3f0bca1997abfcece50235a80ed37d145f851429
Giulia <giuliatullia93@gmail.com>
2021-07-18 17:56:41 +0200

    add-header-jumbo-bozza

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <div class="navbar"></div>
+      <div class="jumbotron d-flex align-items-center justify-content-center flex-column text-light">
+        <h1 class="text-uppercase ">building inspiring spaces</h1>
+        <p class="text-capitalize">we build inspiring residential &amp; commercial spaces</p>
+        <div>
+          <a href="#" class="mx-2 p-2 bg-dark text-light">explore recent work</a>
+          <a href="#" class="mx-2 p-2 bg-warning text-dark">get a quote today</a>
+        </div>
+      </div>
+  </div>
+</template>
+      
+
+<script>
+export default {
+name : 'Header',
+}
+</script>
+
+
+<style lang='scss' scoped>
+@import '@/style/header.scss';
+</style>
+
+    
+    
+    
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1 class="text-success">main</h1>
+  </div>
+</template>
+
+<script>
+export default {
+name : 'Main',
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
